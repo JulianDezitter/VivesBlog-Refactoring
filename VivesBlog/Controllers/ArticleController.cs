ad9e6f7f75cdadd2e44b85d6dfc52a0d16cfde38
JulianDezitter <125573258+JulianDezitter@users.noreply.github.com>
2023-09-18 17:09:20 +0200

    split controllers

---
+++ VivesBlog/Controllers/ArticleController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesBlog.Models;
+using VivesBlog.Services;
+
+namespace VivesBlog.Mvc.Controllers
+{
+    public class ArticleController : Controller
+    {
+        private readonly DbService _dbService;
+
+        public ArticleController(DbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        public IActionResult Details(int id)
+        {
+            var article = _dbService.GetArticle(id);
+
+            return View(article);
+        }
+
+        [HttpGet("Blog/Index")]
+        public IActionResult Index()
+        {
+            var articles = _dbService.GetArticles();
+            return View(articles);
+        }
+
+        [HttpGet("Blog/Create")]
+        public IActionResult BlogCreate()
+        {
+            var articleModel = CreateArticleModel();
+
+            return View(articleModel);
+        }
+
+        [HttpPost("Blog/Create")]
+        public IActionResult BlogCreate(Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                var articleModel = CreateArticleModel(article);
+                return View(articleModel);
+            }
+
+            _dbService.AddArticle(article);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("Blog/Edit/{id}")]
+        public IActionResult BlogEdit(int id)
+        {
+            var article = _dbService.GetArticle(id);
+
+            var articleModel = CreateArticleModel(article);
+
+            return View(articleModel);
+        }
+
+        [HttpPost("Blog/Edit/{id}")]
+        public IActionResult BlogEdit(int id, Article article)
+        {
+            if (!ModelState.IsValid)
+            {
+                var articleModel = CreateArticleModel(article);
+                return View(articleModel);
+            }
+
+            _dbService.UpdateArticle(id, article);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("Blog/Delete/{id}")]
+        public IActionResult BlogDelete(int id)
+        {
+            var article = _dbService.GetArticle(id);
+
+            return View(article);
+        }
+
+        [HttpPost("Blog/Delete/{id}")]
+        public IActionResult BlogDeleteConfirmed(int id)
+        {
+            _dbService.DeleteArticle(id);
+
+            return RedirectToAction("Index");
+        }
+
+        private ArticleModel CreateArticleModel(Article? article = null)
+        {
+            article ??= new Article();
+
+            var authors = _dbService.GetPeopleOrdered();
+
+            var articleModel = new ArticleModel
+            {
+                Article = article,
+                Authors = authors
+            };
+
+            return articleModel;
+        }
+    }
+}
---
+++ VivesBlog/Controllers/PersonController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VivesBlog.Models;
+using VivesBlog.Services;
+
+namespace VivesBlog.Mvc.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly DbService _dbService;
+
+        public PersonController(DbService dbService)
+        {
+            _dbService = dbService;
+        }
+
+        [HttpGet("People/Index")]
+        public IActionResult Index()
+        {
+            var people = _dbService.GetPeople();
+            return View(people);
+        }
+
+        [HttpGet("People/Create")]
+        public IActionResult PeopleCreate()
+        {
+            return View();
+        }
+
+        [HttpPost("People/Create")]
+        public IActionResult PeopleCreate(Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+            _dbService.AddPerson(person);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("People/Edit/{id}")]
+        public IActionResult PeopleEdit(int id)
+        {
+            var person = _dbService.GetPerson(id);
+
+            return View(person);
+        }
+
+        [HttpPost("People/Edit/{id}")]
+        public IActionResult PeopleEdit(int id, Person person)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(person);
+            }
+
+            _dbService.UpdatePerson(id, person);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet("People/Delete/{id}")]
+        public IActionResult PeopleDelete(int id)
+        {
+            var person = _dbService.GetPerson(id);
+
+            return View(person);
+        }
+
+        [HttpPost("People/Delete/{id}")]
+        public IActionResult PeopleDeleteConfirmed(int id)
+        {
+            _dbService.DeletePerson(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
